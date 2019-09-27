9ee9c2e3adcd0f697d2de294a271c59348482d78
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-09-28 01:05:55 +0900

    0928

--- routes/comments.js
+++ routes/comments.js
@@ -2,7 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next)=> {
+router.get('/:id', (req, res, next)=> {
+
+});
+router.patch('/:id',(req,res,next)=>{
+
+});
+router.delete('/:id',(req,res,next)=>{
 
 });
 router.post('/',(req,res,next)=>{
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+/* GET home page. */
+router.get('/', (req, res, next)=> {
 
+});
+router.post('/',(req,res,next)=>{
+
+});
 module.exports = router;
