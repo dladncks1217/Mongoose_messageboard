2d294930bcbcb92036bb5660b1a9d068462779c5
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-10-04 17:52:45 +0900

    comment router 코드수정

--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
 const Comment = require('../schemas/comment');
+const router = express.Router();
+
 // GET/comments/:id
 router.get('/:id', (req, res, next)=> {
     Comment.find({commenter:req.params.id}) //id가 맞는 사람의 id를 가져와야 하므로 이 조건을 추가해주어야 한다.
@@ -10,7 +11,7 @@ router.get('/:id', (req, res, next)=> {
         .catch((error)=>{
             console.error(error);
             next(error);
-        })
+        });
 });
 router.patch('/:id',(req,res,next)=>{  //댓글 수정
     Comment.update({_id:req.params.id},{comment:req.body.comment})  //시퀄라이즈와 인자의 순서가 반대다.
@@ -20,7 +21,7 @@ router.patch('/:id',(req,res,next)=>{  //댓글 수정
         .catch((error)=>{
             console.error(error);
             next(error);
-        })
+        });
 });
 router.delete('/:id',(req,res,next)=>{
     Comment.remove({_id:req.params.id})
@@ -44,6 +45,6 @@ router.post('/',(req,res,next)=>{
         .catch((error)=>{
             console.error(error);
             next(error);
-        })
+        });
 });
 module.exports = router;
