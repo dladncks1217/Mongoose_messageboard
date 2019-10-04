f89244462c33c7c7810cb94ea11741cca53fb5b6
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-10-04 17:53:03 +0900

    index router 코드수정

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
-var express = require('express');
-var router = express.Router();
-const User = require('../schemas/user');  //mongoose는 sequelize와 다르게 모델을 직접 스키마에서 가져온다.(index에서 안가져옴.)
+const express = require('express');
+const User = require('../schemas/user');  //mongoose 는 sequelize 와 다르게 모델을 직접 스키마에서 가져온다.(index 에서 안가져옴.)
 
-
+const router = express.Router();
 /* GET home page. */
 router.get('/', (req, res, next)=> {
-    User.find()    //몽구스는 모두 가져오는게 find()이다. (시퀄라이즈는findAll)
+    User.find({})    //몽구스는 모두 가져오는게 find()이다. (시퀄라이즈는 findAll)
         .then((users)=>{  //몽구스도 promise 지원.
             res.render('mongoose',{users});
         })
@@ -15,3 +14,4 @@ router.get('/', (req, res, next)=> {
         });
 });
 module.exports = router;
+
